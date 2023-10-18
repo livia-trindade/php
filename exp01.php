bc6e5e13539d3fc4b0d3462a896079ac99cf1668
Lívia Trindade <145488817+livia-trindade@users.noreply.github.com>
2023-10-18 13:59:09 -0300

    Add files via upload

---
+++ exp01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+</head>
+<body>
+    <h1>teste de alteração github</h1>
+    
+    <?php
+    echo "teste 01";
+    ?>
+    
+</body>
+</html>
